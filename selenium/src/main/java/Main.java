9c2c9d7c5883accf34d4dfa74218acf65038abd3
rybnikov-dv <DVRybnikov@sberbank.ru>
2022-11-21 14:58:15 +0400

    Initial commit

---
+++ selenium/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://google.com");
+    }
+
+}
